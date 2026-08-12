e47cae1331a5a988a4b27ced6b63435bae26ecca
GLTurtwig <xjondarx@gmail.com>
2026-08-12 18:30:37 -0500

    New Items v0, starting to add slider, added dynamic data instructions

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from 'axios';
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
 
 const NewItems = () => {
+  const [newItems, setNewItems] = useState([]);
+
+  const arrowLeft = "<";
+  const arrowRight = ">";
+
+  useEffect(() => {
+    const fetchNewItems = async () => {
+      try {
+        const response = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems');
+        setNewItems(response.data);
+      } catch (error) {
+        console.error('Error fetching new items:', error);
+      }
+    };
+
+    fetchNewItems();
+  }, []);
+
+  console.log('New Items:', newItems); // Log the fetched new items for debugging
+
+
+  const [sliderRef, instanceRef] = useKeenSlider(
+    {
+      loop: true,
+      mode: 'snap',
+      slides: 
+      {
+        perView: 4,
+        spacing: 10,
+      },
+      breakpoints: {
+        "(max-width: 1200px)": {
+          slides: {
+            perView: 3,
+            spacing: 10,
+          },
+          },
+          "(max-width: 992px)": {
+            slides: {
+              perView: 2,
+              spacing: 10,
+            },
+          },
+          "(max-width: 576px)": {
+            slides: {
+              perView: 1,
+              spacing: 10,
+            }
+          },
+      },
+    },
+    [
+      // add plugins here
+    ]
+  )
+
+  useEffect(() => {
+    if (instanceRef.current) {
+      instanceRef.current.update();
+    }
+  }, [newItems, instanceRef]);
+    
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -14,62 +78,102 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">5h 30m 32s</div>
 
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
+          <div className="slider__wrapper">
+
+            <button
+              className="owl-prev"
+              onClick={() => instanceRef.current?.prev()}
+              type="button"
+            >
+              {arrowLeft}
+            </button>
+
+            <div ref={sliderRef} className="keen-slider">
+              {newItems.map((item, index) => (
+                <div
+                  className="keen-slider__slide"
+                  key={item.id || index}
+                >
+                  <div className="nft__item">
+                    <div className="author_list_pp">
+                      <Link
+                        to={`/author/${item.authorId}`}
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="top"
+                        title="Creator: Monica Lucas"
+                      >
+                        <img
+                          className="lazy"
+                          src={item.authorImage}
+                          alt=""
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+
+                    <div className="de_countdown">5:30:32</div>
+
+                    <div className="nft__item_wrap">
+                      <div className="nft__item_extra">
+                        <div className="nft__item_buttons">
+                          <button type="button">Buy Now</button>
+
+                          <div className="nft__item_share">
+                            <h4>Share</h4>
+
+                            <a href="" target="_blank" rel="noreferrer">
+                              <i className="fa fa-facebook fa-lg"></i>
+                            </a>
+
+                            <a href="" target="_blank" rel="noreferrer">
+                              <i className="fa fa-twitter fa-lg"></i>
+                            </a>
+
+                            <a href="">
+                              <i className="fa fa-envelope fa-lg"></i>
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+
+                      <Link to={`/item-details/${item.id}`}>
+                        <img
+                          src={item.nftImage}
+                          className="lazy nft__item_preview"
+                          alt=""
+                        />
+                      </Link>
+                    </div>
+
+                    <div className="nft__item_info">
+                      <Link to={`/item-details/${item.id}`}>
+                        <h4>{item.title}</h4>
+                      </Link>
+
+                      <div className="nft__item_price">
+                        {item.price} ETH
+                      </div>
+
+                      <div className="nft__item_like">
+                        <i className="fa fa-heart"></i>
+                        <span>{item.likes}</span>
                       </div>
                     </div>
                   </div>
-
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
                 </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>69</span>
-                  </div>
-                </div>
-              </div>
+              ))}
             </div>
-          ))}
+
+            <button
+              className="owl-next"
+              onClick={() => instanceRef.current?.next()}
+              type="button"
+            >
+              {arrowRight}
+            </button>
+
+          </div>
         </div>
       </div>
     </section>
