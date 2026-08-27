5f16994e3cc33668dc09694aca6e60ace7df8013
GLTurtwig <xjondarx@gmail.com>
2026-08-27 01:22:45 -0500

    Added dynamic loading to Item-Details, made sure all cards send the right id, and added a skeleton loading state to ItemDetails.

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -188,7 +188,7 @@ const NewItems = () => {
                     </div>
 
                     <div className="nft__item_info">
-                      <Link to={`/item-details/${item.id}`}>
+                      <Link to={`/item-details/${item.nftId}`}>
                         <h4>{item.title}</h4>
                       </Link>
 
--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,10 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import EthImage from "../images/ethereum.svg";
-import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
-import nftImage from "../images/nftImage.jpg";
+import { Link, useParams } from "react-router-dom";
+import axios from 'axios';
+import Skeleton from "../components/UI/Skeleton";
 
 const ItemDetails = () => {
+
+  const { id } = useParams();
+  const [ itemDetails, setItemDetails ] = useState();
+  const [ loading, setLoading ] = useState(true);
+  
+
+  useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    `https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${id}`
+                );
+                setItemDetails(response.data);
+            } catch (err) {
+                console.error(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -17,43 +40,49 @@ const ItemDetails = () => {
           <div className="container">
             <div className="row">
               <div className="col-md-6 text-center">
-                <img
-                  src={nftImage}
+                {loading ?(
+                  <Skeleton width="540px" height="480px" />
+                )
+                :
+                (<img
+                  src={itemDetails.nftImage}
                   className="img-fluid img-rounded mb-sm-30 nft-image"
                   alt=""
-                />
+                />)}
               </div>
               <div className="col-md-6">
                 <div className="item_info">
-                  <h2>Rainbow Style #194</h2>
+                  {loading ? 
+                  (<Skeleton width="280px" height="42px"/>)
+                  : 
+                  (<h2>{itemDetails.title}</h2>)}
 
                   <div className="item_info_counts">
                     <div className="item_info_views">
                       <i className="fa fa-eye"></i>
-                      100
+                      {itemDetails?.views}
                     </div>
                     <div className="item_info_like">
                       <i className="fa fa-heart"></i>
-                      74
+                      {itemDetails?.likes}
                     </div>
                   </div>
-                  <p>
-                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-                    illo inventore veritatis et quasi architecto beatae vitae
-                    dicta sunt explicabo.
-                  </p>
+                  {loading ? (<Skeleton width="340px" height="90px"/>) :
+                  (<p>
+                    {itemDetails.description}
+                  </p>)}
                   <div className="d-flex flex-row">
                     <div className="mr40">
                       <h6>Owner</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                          <Link to={`/author/${itemDetails?.ownerId}`}>
+                            {loading ? (<Skeleton width="50px" height="50px" borderRadius="50%"/>) : (<img className="lazy" src={itemDetails.ownerImage} alt="" />)}
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          {loading ? (<Skeleton width="160px" height="18px"/>) : (<Link to={`/author/${itemDetails?.ownerId}`}>{itemDetails?.ownerName}</Link>)}
                         </div>
                       </div>
                     </div>
@@ -64,13 +93,13 @@ const ItemDetails = () => {
                       <h6>Creator</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                          <Link to={`/author/${itemDetails?.creatorId}`}>
+                            {loading ? (<Skeleton width="50px" height="50px" borderRadius="50%"/>) : (<img className="lazy" src={itemDetails.creatorImage} alt="" />)}
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          {loading ? (<Skeleton width="160px" height="18px"/>) : (<Link to={`/author/${itemDetails?.creatorId}`}>{itemDetails?.creatorName}</Link>)}
                         </div>
                       </div>
                     </div>
@@ -78,7 +107,7 @@ const ItemDetails = () => {
                     <h6>Price</h6>
                     <div className="nft-item-price">
                       <img src={EthImage} alt="" />
-                      <span>1.85</span>
+                      {loading ? (<Skeleton width="80px" height="48px"/>) : (<span>{itemDetails?.price}</span>)}
                     </div>
                   </div>
                 </div>
