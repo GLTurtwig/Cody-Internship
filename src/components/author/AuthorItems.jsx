5936a623eb1f644ddec9267be6b1f9747213d564
GLTurtwig <xjondarx@gmail.com>
2026-08-26 22:43:18 -0500

    Added the nftId to the link in Author Items

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -61,7 +61,7 @@ const AuthorItems = ({ authorItems = [], loading=true, pfp=""}) => {
                   </Link>
                 </div>
                 <div className="nft__item_info">
-                  <Link to="/item-details">
+                  <Link to={`/item-details/${item.nftId}`}>
                     <h4>{item.title}</h4>
                   </Link>
                   <div className="nft__item_price">{item.price} ETH</div>
